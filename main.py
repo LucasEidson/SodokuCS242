92252d4e91b1fc398c8b26c0eca02ab3ffddca04
Lucas Eidson <leidson@u.rochester.edu>
2025-03-03 16:51:00 -0500

    Improve runtime for backtracking.

--- main.py
+++ main.py
@@ -3,30 +3,25 @@
 import math
 import copy
 
+# import timeit
+
 N = 9  # N is a perfect square representing the dimensions of the board.
 
 
 def main():
     board, unsolved = get_board_input()  # INDEXED BOARD[ROW][COL]
+    # start_time = timeit.default_timer()
     propogate_constraints(board, unsolved)
-    print("Finished Propgating Constraints, Result:")
-    print_board(board)
-    unsolved = get_unsolved(board)
-    if unsolved:
-        print("Backtracking...")
-        backtrack_result = backtrack(board)
-        if backtrack_result != "FAILURE":
-            print("Board Solved!")
-            print_board(backtrack_result)
-        else:
-            print("No Solution.")
+    backtrack_result = backtrack(board, get_mrv_unit(board))
+    if backtrack_result != "FAILURE":
+        print_board(backtrack_result)
     else:
-        print("Board Solved!")
-        print_board(board)
+        print("No Solution.")
+    # print(timeit.default_timer() - start_time)
 
 
-def backtrack(board):
-    cur_unit = get_unsolved_unit(board)
+def backtrack(board, cur_unit):
+    """Use recursive backtracking with forward checking to return a solution to the given board."""
     if cur_unit == None:
         return board
     for possible_val in cur_unit.domain:
@@ -34,28 +29,40 @@ def backtrack(board):
         new_unit = Unit(cur_unit.row, cur_unit.col, possible_val)
         new_board[cur_unit.row][cur_unit.col] = new_unit
         if check_consistency(new_unit, new_board):
-            print_board(new_board)
-            propogate_constraints(new_board, [new_unit])
-            result = backtrack(new_board)
+            neighbors = get_neighbors(cur_unit, new_board)
+            for n in neighbors:
+                get_domain(n, new_board)
+            new_unsolved = get_mrv_unit(new_board)
+            result = backtrack(new_board, new_unsolved)
             if result != "FAILURE":
                 return result
     return "FAILURE"
 
 
 def get_unsolved_unit(board):
-    """Returns an unsolved Unit with the smallest domain or nothing."""
+    """Return an unsolved Unit or nothing."""
     min_domain_size = N + 1
     best_unit = None
     for i in range(N):
         for j in range(N):
             if board[i][j].value == 0:
-                if len(board[i][j].domain) < min_domain_size:
-                    best_unit = board[i][j]
-                    min_domain_size = len(best_unit.domain)
+                return board[i][j]
+
+
+def get_mrv_unit(board):
+    """Return an unsolved unit with the smallest domain or nothing"""
+    min_domain_size = N + 1
+    best_unit = None
+    for i in range(N):
+        for j in range(N):
+            if board[i][j].value == 0 and len(board[i][j].domain) < min_domain_size:
+                min_domain_size = len(board[i][j].domain)
+                best_unit = board[i][j]
     return best_unit
 
 
 def check_consistency(u, board):
+    """Return the consistency of Unit u on the given board."""
     neighbors = get_neighbors(u, board)
     if u.value == 0 or len(u.domain) == 0:
         return False
@@ -65,34 +72,25 @@ def check_consistency(u, board):
     return True
 
 
-def get_unsolved(board):
-    """returns a list of all unsolved units in board."""
-    unsolved = []
-    for i in range(N):
-        for j in range(N):
-            if board[i][j].value == 0:
-                unsolved.append(board[i][j])
-    return unsolved
-
-
 def propogate_constraints(board, unsolved):
-    """Checks consistency between each unit"""
+    """Propogate constraints for all unsolved units and each neighbor of any Unit updated when propogating constraints."""
     units = []
     for u in unsolved:
         units.append(u)
     unsolved = []
     while units:
         u = units.pop()
-        if get_domain(u, board):
+        revised, original = get_domain(u, board)
+        if revised:
             units.extend(get_neighbors(u, board))
 
 
 def get_domain(u, board):  # Constraint Propogation
     """Check constraints on Unit u and change domain accordingly, return True if domain is changed or False if it is not."""
-    domain = u.domain.copy()
-    if len(domain) == 1:
-        u.value = domain[0]
-        return True
+    original = u.domain.copy()
+    if len(original) == 1:
+        u.value = original[0]
+        return False, None
     subgrid_row, subgrid_col = u.get_subgrid_index()
     subgrid_N = int(math.sqrt(N))
     subgrid_row *= subgrid_N
@@ -103,8 +101,8 @@ def get_domain(u, board):  # Constraint Propogation
     neighbors = get_neighbors(u, board)
     for nb in neighbors:
         val = nb.value
-        if val in domain:
-            domain.remove(val)
+        if val in original:
+            original.remove(val)
         if nb.row == u.row:
             if val in unaccounted_row:
                 unaccounted_row.remove(val)
@@ -119,15 +117,15 @@ def get_domain(u, board):  # Constraint Propogation
         ):
             if val in unaccounted_grid:
                 unaccounted_grid.remove(val)
-    revised = u.domain != domain
-    u.domain = domain
+    revised = u.domain != original
+    u.domain = original
     if len(u.domain) == 1:
         u.value = u.domain[0]
-    return revised
+    return revised, original
 
 
 def get_neighbors(u, board):
-    """Returns each other unit which has a binary constraint with Unit u."""
+    """Return each unit on the board which has a binary constraint with Unit u."""
     neighbors = []
     subgrid_row, subgrid_col = u.get_subgrid_index()
     subgrid_N = int(math.sqrt(N))
@@ -150,7 +148,7 @@ def get_neighbors(u, board):
 
 
 def get_board_input():
-    """Return 2D Board List and list of unsolved unit objects created from stdin"""
+    """Return a board represented by a 2D list of Unit objects, and list of all unsolved unit objects created from stdin."""
     board = []
     unsolved = []
     for i in range(N):
@@ -167,16 +165,19 @@ def get_board_input():
 
 
 def print_board(board):
+    """Take a sodoku board represented by a 2D list of Unit objects and print it along with a line of dashes."""
     for i in range(N):
         for j in range(N):
             print(board[i][j].value, end=" ")
         print()
-    for _ in range(2 * N - 1):
+    """for _ in range(2 * N - 1):
         print("-", end="")
-    print()
+    print()"""
 
 
 class Unit:
+    """Represents a Unit (square) on a sodoku board of size N."""
+
     def __init__(self, row, col, value):
         self.row = row
         self.col = col
@@ -184,7 +185,7 @@ class Unit:
         self.value = value
 
     def get_subgrid_index(self):
-        """Returns the index (x, y) of the subgrid of the Variable where an NxN matrix has N sqrt(N)xsqrt(N) subgrids"""
+        """Return the index (x, y) of the subgrid of the Variable where an NxN matrix has N sqrt(N)xsqrt(N) subgrids"""
         subgrid_row = math.floor(self.row / (math.sqrt(N)))
         subgrid_col = math.floor(self.col / (math.sqrt(N)))
         return (subgrid_row, subgrid_col)
